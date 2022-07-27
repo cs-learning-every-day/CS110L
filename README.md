8d30003c55dd4a7430c4f1467b8f71d5b87484e7
XmchxUp <1394466835@qq.com>
2022-07-27 20:49:54 +0800

    finish week3: part 2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 - [X] Week 1 Exercises: Hello world
 - [X] Week 2 Exercises: Ownership and structs
-- [ ] Week 3 Exercises: Error handling, I/O, and traits
+- [X] Week 3 Exercises: Error handling, I/O, and traits
 - [ ] Project 1: The DEET Debugger
 - [ ] Week 4 Exercises: Checking in
 - [ ] Week 5 Exercises: Farm meets multithreading
--- code/week3/linked_list/src/main.rs
+++ code/week3/linked_list/src/main.rs
@@ -1,4 +1,6 @@
 use linked_list::LinkedList;
+
+use crate::linked_list::ComputeNorm;
 pub mod linked_list;
 
 fn main() {
@@ -15,8 +17,36 @@ fn main() {
     println!("size: {}", list.get_size());
     println!("{}", list.to_string()); // ToString impl for anything impl Display
 
+    let mut list_clone = list.clone();
+    list_clone.push_front(1);
+    println!("clone list and push front 1: {}", list_clone);
+    println!("origin list: {}", list);
+
+    let mut l2: LinkedList<String> = LinkedList::new();
+    let hl = String::from("hello");
+    l2.push_front(hl);
+    println!("{}", l2);
+
+    let mut l3 = l2.clone();
+    println!("test equal: {}", l3 == l2);
+
+    let mut tmp = l3.pop_front().unwrap();
+    tmp.push_str("string");
+    println!("test equal: {}", l3 == l2);
+    println!(
+        "deep clone: old: {},new:{},l3:{}",
+        l2.pop_front().unwrap(),
+        tmp,
+        l3
+    );
+
+    let mut flst: LinkedList<f64> = LinkedList::new();
+    flst.push_front(3.0);
+    flst.push_front(4.0);
     // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    for val in &flst {
+        println!("{}", val);
+    }
+
+    println!("{}", flst.compute_norm());
 }
